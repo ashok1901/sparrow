b1be90c1e6c8d39dc63e09cfb8994307647acbc8
Ashok Kumar <kumara@integral.com>
2015-07-22 11:46:12 -0700

    Adding another example of Scanner

--- algoSolutions/src/main/java/com/sparrow/algoSolutions/algos/ScannerUsage.java
+++ algoSolutions/src/main/java/com/sparrow/algoSolutions/algos/ScannerUsage.java
@@ -2,36 +2,63 @@ package com.sparrow.algoSolutions.algos;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ScannerUsage 
 {
     public static void main(String[] args) {
-
+        // Read all lines, line by line
         try {
             // This is how this is used in hacker rank
             //Scanner sc = new Scanner(new File(System.in));
-            String inputFile = "C:\\test\\scannerReader.txt";
+            String inputFile = "C:\\test\\scannerReader1.txt";
             Scanner sc = new Scanner(new File(inputFile));
-            String nStr = sc.nextLine();
-            int n = Integer.parseInt(nStr);
-            String dataStr = sc.nextLine();
-            String[] dataTokens = dataStr.split("\\s+");
-            int[] data = new int[dataTokens.length];
-            for (int i = 0; i < dataTokens.length; i++) {
-                data[i] = Integer.parseInt(dataTokens[i]);
+            List<String> data = new ArrayList<String>();
+            while(sc.hasNextLine()) {
+                data.add(sc.nextLine());
             }
 
-            System.out.println(n);
-            for (int x : data) {
-                System.out.print(x);
-                System.out.print(" ");
+            if (data.isEmpty()) {
+                System.out.println("=== No Input ===");
+            } else {
+                for (String x : data) {
+                    System.out.println(x);
+                }
             }
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
+
+//    public static void main(String[] args) {
+//
+//        try {
+//            // This is how this is used in hacker rank
+//            //Scanner sc = new Scanner(new File(System.in));
+//            String inputFile = "C:\\test\\scannerReader.txt";
+//            Scanner sc = new Scanner(new File(inputFile));
+//            String nStr = sc.nextLine();
+//            int n = Integer.parseInt(nStr);
+//            String dataStr = sc.nextLine();
+//            String[] dataTokens = dataStr.split("\\s+");
+//            int[] data = new int[dataTokens.length];
+//            for (int i = 0; i < dataTokens.length; i++) {
+//                data[i] = Integer.parseInt(dataTokens[i]);
+//            }
+//
+//            System.out.println(n);
+//            for (int x : data) {
+//                System.out.print(x);
+//                System.out.print(" ");
+//            }
+//        } catch (FileNotFoundException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//    }
 
 //    public static void main(String[] args) {
 //        String inputFile = "C:\\test\\scannerReader.txt";
